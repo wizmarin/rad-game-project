16487cc539979d82eeba6ed81a9b1321d05daa7b
wizmarin <sparkle.shrimp.prog@gmail.com>
2025-08-10 21:02:31 +0100

    Added animator, physics and movment script to the player

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Animator player;
+    Rigidbody body;
+
+    float speed;
+    float walkSpeed = 3;
+    float runMultiplier = 3;
+    float turningSpeed = 180;
+
+    float jumpForce = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInChildren<Animator>();
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.SetBool("IsWalking", false);
+        speed = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed = walkSpeed;
+            player.SetBool("IsWalking", true);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            speed = -walkSpeed;
+            player.SetBool("IsWalking", true);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+            player.SetBool("IsWalking", true);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, -turningSpeed * Time.deltaTime);
+            player.SetBool("IsWalking", true);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed *= runMultiplier;
+            player.SetBool("IsRunning", true);
+        }
+        else
+        {
+            player.SetBool("IsRunning", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+            player.SetTrigger("Jump");
+        }
+        else
+        {
+            player.ResetTrigger("Jump");
+        }
+
+        transform.position += speed * transform.forward * Time.deltaTime;
+    }
+}
